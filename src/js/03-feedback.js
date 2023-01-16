5a28fc7697a3b59cc2be67d7f2c456166988626e
Igor Bondarchuk <bondatchuk1989@gmail.com>
2023-01-16 21:56:45 +0200

    fixed some bugs

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,4 +1,5 @@
 import throttle from 'lodash.throttle';
+import { save, load, remove } from './storage';
 
 const ref = {
   form: document.querySelector('.feedback-form'),
@@ -12,22 +13,11 @@ const fieldValue = {};
 ref.form.addEventListener('input', throttle(hendleEventForm, 1000));
 ref.form.addEventListener('submit', hendleEventSubmit);
 
-fillingFildFromStorage();
-
-function fillingFildFromStorage() {
-  const getMessage = localStorage.getItem(LOCALSTORAGE_KEY);
-
-  if (getMessage) {
-    const object = JSON.parse(getMessage);
-
-    ref.outEmai.value = object.email !== undefined ? object.email : '';
-    ref.outMessage.value = object.message !== undefined ? object.message : '';
-  }
-}
+fillingFormFromLS();
 
 function hendleEventForm(eve) {
   fieldValue[eve.target.name] = eve.target.value;
-  saveInLocalStorage(fieldValue);
+  save(LOCALSTORAGE_KEY, fieldValue);
 }
 
 function hendleEventSubmit(eve) {
@@ -38,10 +28,14 @@ function hendleEventSubmit(eve) {
       [eve.target.elements.message.name]: eve.target.elements.message.value,
     });
     eve.target.reset();
-    localStorage.removeItem(LOCALSTORAGE_KEY);
+    remove(LOCALSTORAGE_KEY);
   }
 }
 
-function saveInLocalStorage(fieldValue) {
-  localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(fieldValue));
+function fillingFormFromLS() {
+  const object = load(LOCALSTORAGE_KEY);
+  if (object !== undefined) {
+    ref.outEmai.value = object.email !== undefined ? object.email : '';
+    ref.outMessage.value = object.message !== undefined ? object.message : '';
+  }
 }
